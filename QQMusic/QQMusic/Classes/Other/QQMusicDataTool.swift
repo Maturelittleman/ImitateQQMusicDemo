fbc80cca3e09405dbdc474e7eac476d1d904960d
仲琦 <601582657@qq.com>
2016-05-18 16:47:33 +0800

    创建一个数据获取的工具类,用来获取plist文件内的数据

--- QQMusic/QQMusic/Classes/Other/QQMusicDataTool.swift
+++ QQMusic/QQMusic/Classes/Other/QQMusicDataTool.swift
@@ -11,7 +11,28 @@ import UIKit
 class QQMusicDataTool: NSObject {
     
 
-    
-
-
+    class func getMusicData(result: (musicMs: [QQListModel]) -> ()) {
+        
+        //获取文件路径
+        guard let path = NSBundle.mainBundle().pathForResource("Musics.plist", ofType: nil) else {
+            result(musicMs: [QQListModel]())
+            return
+        }
+        
+        //取出文件中的数据
+        guard let dictArray = NSArray(contentsOfFile: path) else {
+            result(musicMs: [QQListModel]())
+            return
+        }
+        
+        //字典转模型
+        var resultMs = [QQListModel]()
+        for dict in dictArray {
+            let MusicM = QQListModel(dict: dict as! [String: AnyObject])
+            resultMs.append(MusicM)
+        }
+        
+        //给出去
+        result(musicMs: resultMs)
+    }
 }
--- QQMusic/QQMusic/Classes/QQList/Controller/QQListTVC.swift
+++ QQMusic/QQMusic/Classes/QQList/Controller/QQListTVC.swift
@@ -16,11 +16,10 @@ class QQListTVC: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        QQMusicDataTool.getMusicData { (musicMs) -> () in
+            
+            print(musicMs)
+        }
     }
 
     override func didReceiveMemoryWarning() {
